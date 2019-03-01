0cdb086af6f582f5864e014f6d0efdb129bd399f
codecademy <ccuser@codecademy.com>
2019-03-01 04:47:53 +0000

    Added function to generate message for traveler and present attractions they might be interested in.

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 # ALL LOCAL VARIABLES
 # initialize lists
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "So Paulo, Brazil", "Cairo, Egypt"];
-test_traveler = ['Erin Wilkes', 'Shanghai, China', 'historical site', 'art'];
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']];
 
 # list of attractions
 attractions = [[] for index in range(0, len(destinations))];
@@ -36,7 +36,7 @@ def add_attraction(destination, attraction):
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']]);
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
-add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
 add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
 add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
 add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
@@ -51,7 +51,7 @@ def find_attractions(destination, interests):
   # attractions in the destination city
   attractions_in_city = attractions[destination_index];
   attractions_with_interests = [];
-  
+  # compare each attraction tag with the list of interests. If matched, append said attraction.
   for possible_attraction in attractions_in_city:
     attraction_tags = possible_attraction[1];
     for interest in interests:
@@ -59,8 +59,35 @@ def find_attractions(destination, interests):
         attractions_with_interests.append(possible_attraction[0]);
   return attractions_with_interests;
 
+# tests
 la_arts = find_attractions("Los Angeles, USA", ['art']);
 print(la_arts);
+
+# main event
+def get_attraction_for_traveler(traveler):
+  traveler_destination = traveler[1];
+  traveler_interests = traveler[2];
+  traveler_attractions = find_attractions(traveler_destination, traveler_interests);
+  
+  interests_string = "Hi ";
+  interests_string += traveler[0];
+  interests_string += ", we think you'll like these places around ";
+  interests_string += traveler[1] + ": ";
+  
+  # for attraction in traveler_attractions:
+  for index in range(0, len(traveler_attractions)):
+    if (index < len(traveler_attractions)-1):
+      interests_string += traveler_attractions[index] + ", ";
+    if (index == len(traveler_attractions)-1):
+      interests_string += "and the " + traveler_attractions[index] + ".";
+  return interests_string;
+
+smills_france = get_attraction_for_traveler(['Dereck Smill', 'Paris, France', ['monument', 'art']]);
+
+print(smills_france);
+  
+  
+
     
     
     
